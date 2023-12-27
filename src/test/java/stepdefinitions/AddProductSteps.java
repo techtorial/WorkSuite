8b6861771d731ddf29c61b346830e9ad2d91bf0f
Gino <153547103+Gino198701@users.noreply.github.com>
2023-12-26 19:27:34 -0600

    TWP-8

--- src/test/java/stepdefinitions/AddProductSteps.java
+++ src/test/java/stepdefinitions/AddProductSteps.java
@@ -4,6 +4,7 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
+import pages.HomePage;
 import pages.LoginPage;
 import pages.ProductsPage;
 import utils.DriverHelper;
@@ -12,12 +13,12 @@ public class AddProductSteps {
 
     WebDriver driver = DriverHelper.getDriver();
     ProductsPage productsPage = new ProductsPage(driver);
+    HomePage homePage = new HomePage(driver);
 
 
-    @Then("the user clicks the Products from menu option and AddProduct button")
-    public void the_user_clicks_the_products_from_menu_option_and_add_product_button() throws InterruptedException {
-        productsPage.clickProductAndAddProductButtons();
-
+    @Then("the user clicks the {string} from menu option")
+    public void the_user_clicks_the_from_menu_option(String buttonName) {
+        homePage.clickMainMenu(buttonName);
     }
 
     @Then("the user enter {string} price {string}")
@@ -25,8 +26,8 @@ public class AddProductSteps {
         productsPage.enterProductNameAndPrice(name, price);
     }
 
-    @Then("the user select product from the product category, product Sub category, tax, and select client can purschase")
-    public void the_user_select_product_from_the_product_category_product_sub_category_tax_and_select_client_can_purschase() throws InterruptedException {
+    @Then("the user select product from the product category, product Sub category, tax, and select client can purchase")
+    public void the_user_select_product_from_the_product_category_product_sub_category_tax_and_select_client_can_purchase() throws InterruptedException {
         productsPage.selectionOfCategoryAndTax();
     }
 
@@ -35,10 +36,11 @@ public class AddProductSteps {
         productsPage.addingTextToDescription("very good product");
     }
 
-    //   @Then("user upload picture of the product")
-    //   public void user_upload_picture_of_the_product() throws InterruptedException {
-    //       productsPage.uploadingPictureOfTheProduct();
-    //   }
+//    @Then("user upload picture of the product")
+//    public void user_upload_picture_of_the_product() throws InterruptedException {
+//        productsPage.uploadingPictureOfTheProduct();
+//    }
+
     @Then("then user clicks save button")
     public void then_user_clicks_save_button() throws InterruptedException {
         productsPage.clickSaveButton();
@@ -46,9 +48,9 @@ public class AddProductSteps {
 
     @Then("user validates the product name on the page")
     public void user_validates_the_product_name_on_the_page() throws InterruptedException {
-        productsPage.validationOfTheProduct("Truck-Mounted" +" "+"Crane");
+        productsPage.validationOfTheProduct("Truck-Mounted" + " " + "Crane");
     }
 
 
-    }
+}
 
