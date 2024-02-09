c84ae2261fe04e06b52ebd4a63b8f2b3de3bbdd6
davidcitak <edavidcitak@gmail.com>
2024-02-09 12:34:24 -0600

    Selenium Grid changes

--- src/test/java/runners/CucumberTestRunner.java
+++ src/test/java/runners/CucumberTestRunner.java
@@ -6,12 +6,15 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/test/resources/features/apifeature",//feature files
+        features = "src/test/resources/features/AddNewClient.feature",//feature files
         glue = "", //step definitions
         dryRun = false,//getting snips without executing and updating test. 
         plugin = {"pretty","html:target/uiReport.html","rerun:target/uiFailedTests.txt","json:target/cucumber-report.json"}
 )
 public class CucumberTestRunner {
 
+    // We must have maven installed in our computer.
+    // we need to use mvn clean test
+    // We need update our pom.xml file. We should add maven-surefire-plugin
 
 }
